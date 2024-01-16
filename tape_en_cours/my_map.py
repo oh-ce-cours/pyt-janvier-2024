4cf623af3d14b3fa0f1545a3ff05765d82515fb5
Matthieu Falce <matthieu@falce.net>
2024-01-16 14:22:04 +0100

    gitautosync update 90

--- tape_en_cours/my_map.py
+++ tape_en_cours/my_map.py
@@ -4,7 +4,7 @@ type1 = typing.TypeVar("type1")
 type2 = typing.TypeVar("type2")
 
 
-def my_map(func, elements: list[type1]) -> list[type2]:
+def my_map(func: typing.Callable[[type1], type2], elements: list[type1]) -> list[type2]:
     res = []
     for element in elements:
         tmp = func(element)
