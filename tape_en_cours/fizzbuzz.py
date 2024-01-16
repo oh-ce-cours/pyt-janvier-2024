15d9b44afb1cf0fa82b2b4c42d0f185bdcafba1b
Matthieu Falce <matthieu@falce.net>
2024-01-16 11:35:09 +0100

    gitautosync update 57

--- tape_en_cours/fizzbuzz.py
+++ tape_en_cours/fizzbuzz.py
@@ -1,7 +1,7 @@
 def fizzbuzz1():
     for num in range(1, 101):
         res = None
-        if num % 3 == 0 and num % 5 == 0:
+        if not num % 3 and not num % 5:
             res = "FizzBuzz"
         elif num % 3 == 0:
             res = "Fizz"
