5868d491dc0152d2fbf6bce66044dded3b0f56aa
Matthieu Falce <matthieu@falce.net>
2024-01-17 16:59:52 +0100

    gitautosync update 255

--- tape_en_cours/exceptions.py
+++ tape_en_cours/exceptions.py
@@ -1,11 +1,14 @@
 try:
-print("peut lever une exception") raise AssertionError()
+    print("peut lever une exception")
+    raise AssertionError()
 except AssertionError as e:
-print(" gère l'exception AssertionError")
-except (IndexError, ArithmeticError) as e: print(" gère d'autres exceptions")
+    print(" gère l'exception AssertionError")
+except (IndexError, ArithmeticError) as e:
+    print(" gère d'autres exceptions")
 except Exception as e:
-print(" gère le reste des exceptions")
+    print(" gère le reste des exceptions")
 else:
-print("suite logique du code qui peut lever une exception") print("mais qui n'en lève pas lui-même")
+    print("suite logique du code qui peut lever une exception")
+    print("mais qui n'en lève pas lui-même")
 finally:
-print("appelé quel que soit le parcours d'exception")
+    print("appelé quel que soit le parcours d'exception")
