16d36281a86bd54e4326991807e7ba5974cc03d9
Matthieu Falce <matthieu@falce.net>
2024-01-17 17:11:35 +0100

    gitautosync update 268

--- tape_en_cours/exceptions.py
+++ tape_en_cours/exceptions.py
@@ -4,7 +4,7 @@ import sys
 def test2():
     try:
         return 1 + "1"
-    except:
+    except IndexError:
         return "exception"
     finally:
         print("dans finally", sys.exc_info())
