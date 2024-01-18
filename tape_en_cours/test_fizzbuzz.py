ed2e1d3255d307b56bf589507cd81472a3598fc1
Matthieu Falce <matthieu@falce.net>
2024-01-18 10:34:09 +0100

    gitautosync update 330

--- tape_en_cours/test_fizzbuzz.py
+++ tape_en_cours/test_fizzbuzz.py
@@ -15,6 +15,10 @@ class TestFizzBuzz(unittest.TestCase):
     def test_other(self):
         self.assertEqual(fizzbuzz_element(2), "3")
 
+    def test_wrong_type(self):
+        with self.assertRaises(TypeError):
+            fizzbuzz_element("2")
+
 
 if __name__ == "__main__":
     unittest.main()
