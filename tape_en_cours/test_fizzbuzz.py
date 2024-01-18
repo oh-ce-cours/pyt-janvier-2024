548f5cd0217728ff4af18b6b8311f3c34bac5764
Matthieu Falce <matthieu@falce.net>
2024-01-18 10:21:55 +0100

    gitautosync update 323

--- tape_en_cours/test_fizzbuzz.py
+++ tape_en_cours/test_fizzbuzz.py
@@ -5,3 +5,7 @@ import unittest
 class TestFizzBuzz(unittest.TestCase):
     def test_fizz(self):
         self.assertEqual(fizzbuzz_element(4), "fizz")
+
+
+if __name__ == "__main__":
+    unittest.main()
