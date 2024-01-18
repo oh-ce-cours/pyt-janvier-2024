372a62eaca98e32ebe4f3bf9571ba183ef05ef20
Matthieu Falce <matthieu@falce.net>
2024-01-18 15:46:51 +0100

    gitautosync update 378

--- tape_en_cours/demo_flask.py
+++ tape_en_cours/demo_flask.py
@@ -1,6 +1,17 @@
-from flask import Flask app = Flask(__name__)
-@app.route("/") def home():
-return "Hello, world!"
-@app.route("/about") def about():
-return "This is the about page."
-if __name__ == "__main__": app.run()
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Hello, world!"
+
+
+@app.route("/about")
+def about():
+    return "This is the about page."
+
+
+if __name__ == "__main__":
+    app.run()
